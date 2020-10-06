a30be72e3c2241bfcab8622e5866ea43261e51d6
Jonathan Diaz <jonathandiaz0914@gmail.com>
2020-10-06 17:49:31 -0400

    deploy ready

--- client/src/components/Offer.js
+++ client/src/components/Offer.js
@@ -10,13 +10,13 @@ const Offer = () => {
       <Bounce>
         <div className='offer__description'>
           Full Stack Web Development using MongoDB, Express, React, Node.js
-          (MERN) with an emphasis on Backend web development. Experienced in
+          (MERN) with an emphasis on Frontend web development. Experienced in
           building CRUD applications, RESTful APIs using Node.js and express,
           User Auth and oAuth with PassportJS and JWT. Also have experience with
           Firebase, Firestore, React Context Api . Common libraries of use:
-          Axios, Redux, Mongoose, Lodash, Express, body-parser, react-router.
-          Common use of UI libraries: Bootstrap 4, React-Bootstrap,
-          MaterializeCSS, Material-UI.
+          Axios, Redux, Mongoose, Express, body-parser, react-router. Common use
+          of UI libraries: Bootstrap 4, React-Bootstrap, MaterializeCSS,
+          Material-UI.
           <br />
           <a href='/uploads/pdf/resume.pdf'>
             <button>View Resume</button>
